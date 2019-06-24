4da6e8082eb045d511ce94b831d61ff350de457b
salubrious-chain-5511 <anisa.bottoms@gmail.com>
2019-06-24 16:53:38 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,42 @@
+var katzDeliLine = [] 
+katzDeliLine [4] = ["Ada",
+                   "Steven",
+                   "Blake",
+                   "Avi"]; {} 
+ }
+  function takeANumber(katzDeliLine,name) {
+  katzDeliLine.push(name) 
+  return ("Welcome" + name + "You are number")
+  } 
+  function takeANumber() = "You are number";
+    console.log = "Welcome"
+    return ("Welcome" + "katzDeliLine[0]" + "takeANumber" + "1"); }
+ {
+  function takeANumber() = ("You are number"); }
+    console.log = "Welcome"
+    return ("Welcome" + "katzDeliLine[1]" + "takeANumber" + "2"); }
+  {
+  function takeANumber() = ("You are number"); }
+    console.log = "Welcome"
+    return ("Welcome" + "katzDeliLine[2]" + "takeANumber" + "3"); }
+  } 
+  function takeANumber() = ("You are number"); }
+    console.log = "Welcome"
+    return ("Welcome" + "katzDeliLine[3]s" + "takeANumber" + "4"); }
+  }
+  {
+  function nowServing () = "Now serving"
+     console.log = "katzDeliLine"
+     return ("nowServing" + "katzDeliLine [0-3]")
+  }
+  {
+  function currentLine () = "The line is currently"
+     var (3) = ["Bill", "Jan", "Ann"]
+     return ("currentLine" + "[0-2]")
+  }
+  
+  
+  
+
+  
+  
